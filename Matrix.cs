8fdcb1360565716a5263399ca71dc3265f6f280f
Dominik <62477432+imdominiq@users.noreply.github.com>
2023-10-25 22:12:50 +0200

    Add files via upload

---
+++ Matrix.cs
@@ -0,0 +1,87 @@
+﻿using System;
+namespace Macierz
+{
+    class Matrix
+    {
+        private double[,] data;
+        public int Rows { get; private set; }
+        public int Columns { get; private set; }
+
+        public Matrix(int rows, int columns)
+        {
+            Rows = rows;
+            Columns = columns;
+            data = new double[rows, columns];
+        }
+
+        public double this[int row, int col]
+        {
+            get { return data[row, col]; }
+            set { data[row, col] = value; }
+        }
+
+        public static Matrix operator +(Matrix matrix1, Matrix matrix2)
+        {
+            Matrix result = new Matrix(matrix1.Rows, matrix1.Columns);
+
+            for (int i = 0; i < matrix1.Rows; i++)
+            {
+                for (int j = 0; j < matrix1.Columns; j++)
+                {
+                    result[i, j] = matrix1[i, j] + matrix2[i, j];
+                }
+            }
+
+            return result;
+        }
+
+        public static Matrix operator *(Matrix matrix1, Matrix matrix2)
+        {
+
+            Matrix result = new Matrix(matrix1.Rows, matrix2.Columns);
+
+            for (int i = 0; i < matrix1.Rows; i++)
+            {
+                for (int j = 0; j < matrix2.Columns; j++)
+                {
+                    double sum = 0;
+                    for (int k = 0; k < matrix1.Columns; k++)
+                    {
+                        sum += matrix1[i, k] * matrix2[k, j];
+                        result[i, j] = sum;
+                    }
+                }
+            }
+
+            return result;
+        }
+
+        public Matrix Transpose()
+        {
+            Matrix transposedMatrix = new Matrix(Columns, Rows);
+
+            for (int i = 0; i < Rows; i++)
+            {
+                for (int j = 0; j < Columns; j++)
+                {
+                    transposedMatrix[j, i] = data[i, j];
+                }
+            }
+
+            return transposedMatrix;
+        }
+
+        public void Print()
+        {
+            for (int i = 0; i < Rows; i++)
+            {
+                for (int j = 0; j < Columns; j++)
+                {
+                    Console.Write(data[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,62 @@
+﻿namespace Macierz
+{
+    class Program
+    {
+        static void Main()
+        {
+            bool isFinished = false;
+            int columns = 0;
+            int rows = 0;
+
+            while (isFinished == false)
+            {
+                Console.WriteLine("Podaj ilość kolumn dla macierzy");
+                columns = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Podaj ilość wierszy dla macierzy");
+                rows = Convert.ToInt32(Console.ReadLine());
+
+                Matrix matrix1 = new Matrix(rows, columns);
+                Matrix matrix2 = new Matrix(rows, columns);
+
+                for (int i = 0; i < matrix1.Rows; i++)
+                {
+                    for (int j = 0; j < matrix1.Columns; j++)
+                    {
+                        Console.WriteLine("Podaj wartość pola [" + i + "," + j + "] dla macierzy 1");
+                        matrix1[i, j] = Convert.ToInt32(Console.ReadLine());
+                    }
+                }
+
+                Console.WriteLine("Macierz 1");
+                matrix1.Print();
+
+                for (int i = 0; i < matrix2.Rows; i++)
+                {
+                    for (int j = 0; j < matrix2.Columns; j++)
+                    {
+                        Console.WriteLine("Podaj wartość pola [" + i + "," + j + "] dla macierzy 2");
+                        matrix2[i, j] = Convert.ToInt32(Console.ReadLine());
+                    }
+                }
+
+                Console.WriteLine("Macierz 2");
+                matrix2.Print();
+
+                Matrix sum = matrix1 + matrix2;
+                Console.WriteLine("Suma macierzy");
+                sum.Print();
+
+                Matrix mult = matrix1 * matrix2;
+                Console.WriteLine("Iloczyn macierzy");
+                mult.Print();
+
+                Matrix transpodsedMatirx1 = matrix1.Transpose();
+                Console.WriteLine("Transpozycja macierzy1");
+                transpodsedMatirx1.Print();
+                Console.ReadLine();
+                isFinished = true;
+
+            }
+        }
+    }
+}
